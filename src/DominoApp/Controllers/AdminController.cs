edaeb4241c4e2308eaaced08c3513d563e6614e8
Johann <jwjosefy@gmail.com>
2016-04-28 23:18:00 -0300

    Estrutura de projetos Data, DomainModel e Services

---
+++ src/DominoApp/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DominoApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return RedirectToAction("dashboard");
+        }
+
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
