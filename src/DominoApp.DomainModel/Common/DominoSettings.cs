3f25edbd5611b119708a7d775977ce6f410a0a41
Johann <jwjosefy@gmail.com>
2016-05-03 05:11:01 -0300

    Vinculo do repo no controller BO, classe DominoSettings

---
+++ src/DominoApp.DomainModel/Common/DominoSettings.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominoApp.DomainModel.Common
+{
+    /// <summary>
+    /// Modelo das configurações do Backoffice
+    /// </summary>
+    /// <remarks>
+    /// Esta classe representa o modelo que irá guardar todas
+    /// as configurações do sistema
+    /// </remarks>
+    public class DominoSettings
+    {
+        /// <summary>
+        /// Pontuação que o usuário ganhará ao se cadastrar no jogo
+        /// </summary>
+        public int BonusNovoCadastro { get; set; }
+        
+        public int LoginStreakBonus { get; set; }
+
+        public int LoginStreakQtd { get; set; }
+
+        public int BonusEmailAmigo { get; set; }
+
+        public int VitoriaSeguidaBonus { get; set; }
+
+        public int VitoriaSeguidaQtd { get; set; }
+
+        public int CompraMinimaRemovePropaganda { get; set; }
+
+        public int DiasSemPropaganda { get; set; }
+    }
+}
--- src/DominoApp/Controllers/AdminController.cs
+++ src/DominoApp/Controllers/AdminController.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DominoApp.Services;
+
 
 namespace DominoApp.Controllers
 {
     public class AdminController : Controller
     {
+        private BackOfficeRepository _backOfficeRepository;
+
+        public AdminController()
+        {
+                this._backOfficeRepository = new BackOfficeRepository();
+        }
+
         // GET: Admin
         public ActionResult Index()
         {
