5644c1dac9c31867fb11625b195de909fa76829e
Johann <jwjosefy@gmail.com>
2016-05-03 05:13:58 -0300

    Métodos base no BO repo

--- src/DominoApp.Services/BackOfficeRepository.cs
+++ src/DominoApp.Services/BackOfficeRepository.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DominoApp.DomainModel.Common;
 
 namespace DominoApp.Services
 {
     public class BackOfficeRepository
     {
+        public DominoSettings GetDominoSettings()
+        {
+            throw new NotImplementedException("IMPLEMENTAR O ACESSO AO BD");
+        }
+
+        public void SaveDominoSettings(DominoSettings settings)
+        {
+            throw new NotImplementedException("Criar gravação no BD");
+        }
 
     }
 }
