94d95931169437fa5f33b126acdcb1802e77d874
Johann <jwjosefy@gmail.com>
2016-05-03 04:30:36 -0300

    Referência entre projetos, repositório BackOffice

--- src/DominoApp.Services/BackOfficeRepository.cs
+++ src/DominoApp.Services/BackOfficeRepository.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DominoApp.DomainModel
+namespace DominoApp.Services
 {
-    public class Class1
+    public class BackOfficeRepository
     {
+
     }
 }
