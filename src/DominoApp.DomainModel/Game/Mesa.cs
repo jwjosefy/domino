344e2c8b7f856e0ecf70284525ed14f747d1bd9c
Johann <jwjosefy@gmail.com>
2016-06-13 02:14:46 -0300

    Classes do motor de jogo do Dominó

---
+++ src/DominoApp.DomainModel/Game/Mesa.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominoApp.DomainModel.Game
+{
+    /// <summary>
+    /// Mesa precisa controlar a sequencia das pedras jogadas
+    /// e quais são as pontas esquerda e direita.
+    /// Também precisa controlar a pilha de pedras para comprar.
+    /// </summary>
+    public class Mesa
+    {
+        #region Propriedades
+
+        /// <summary>
+        /// Quais foram as pedras jogadas (uso interno)
+        /// </summary>
+        private LinkedList<Pedra> _pedrasJogadas;
+
+        /// <summary>
+        /// Quais foram as pedras jogadas
+        /// </summary>
+        /// <remarks>Não permite que classes externas modifiquem a mesa</remarks>
+        public IReadOnlyList<Pedra> PedrasJogadas => _pedrasJogadas.ToList();
+
+        /// <summary>
+        /// Monte de pedras que podem ser compradas
+        /// </summary>
+        public Stack<Pedra> Monte { get; set; }
+
+        public int PontaEsquerda => ObterPonta(Lado.Esquerdo);
+        public int PontaDireita => ObterPonta(Lado.Direito);
+
+        #endregion
+
+        public Mesa()
+        {
+            IniciarMesa();
+        }
+
+        /// <summary>
+        /// (Re)inicia a mesa
+        /// </summary>
+        public void IniciarMesa()
+        {
+            _pedrasJogadas = new LinkedList<Pedra>();
+
+            // inicia as 28 pedras
+            var pedras = new List<Pedra>();
+
+            for (var i = 0; i <= 6; i++)
+                for (var j = i; j <= 6; j++)
+                    pedras.Add(new Pedra(i, j));
+
+            // coloca as 28 pedras misturadas
+            var r = new Random();
+            Monte = new Stack<Pedra>(pedras.OrderBy(p => r.Next()));
+        }
+
+        /// <summary>
+        /// Usada para iniciar a mão dos jogadores ou qnd não tem mais pedras
+        /// </summary>
+        /// <returns></returns>
+        public Pedra ComprarPedra()
+        {
+            return Monte.Count == 0 ? null : Monte.Pop();
+        }
+
+        /// <summary>
+        /// Executa uma jogada, adicionando a pedra na mesa em alguma ponta
+        /// </summary>
+        /// <param name="pedra">Qual é a pedra</param>
+        /// <param name="lado">Em qual ponta a pedra será jogada</param>
+        public void Jogar(Pedra pedra, Lado lado)
+        {
+            var ponta = ObterPonta(lado);
+
+            if (!pedra.PodeJogar(ponta))
+                throw new InvalidOperationException($"Não pode jogar a pedra {pedra} numa ponta {ponta}");
+
+            if (lado == Lado.Esquerdo)
+            {
+                _pedrasJogadas.AddFirst(pedra);
+            }
+            else
+            {
+                _pedrasJogadas.AddLast(pedra);
+            }
+        }
+
+        private int ObterPonta(Lado lado)
+        {
+            if (_pedrasJogadas.Count == 0)
+                return -1;
+
+            if (_pedrasJogadas.Count == 1)
+            {
+                var pedra = _pedrasJogadas.First.Value;
+
+                // Sempre devolve o maior lado como sendo a ponta da esquerda
+                // para iniciar o jogo
+
+                return lado == Lado.Esquerdo ? pedra.Lado2 : pedra.Lado1;
+            }
+
+            int ponta;
+
+            if (lado == Lado.Esquerdo)
+            {
+                var pedraPonta = _pedrasJogadas.First.Value;
+                ponta = pedraPonta.PontaLivre(_pedrasJogadas.First.Next?.Value);
+            }
+            else
+            {
+                var pedraPonta = _pedrasJogadas.Last.Value;
+                ponta = pedraPonta.PontaLivre(_pedrasJogadas.Last.Previous?.Value);
+            }
+
+            return ponta;
+        }
+
+        /// <summary>
+        /// Pontas da mesa de dominó
+        /// </summary>
+        public enum Lado
+        {
+            Esquerdo,
+            Direito
+        }
+    }
+}
---
+++ src/DominoApp.DomainModel/Game/Pedra.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominoApp.DomainModel.Game
+{
+    public class Pedra
+    {
+        public int Lado1 { get; private set; }
+        public int Lado2 { get; private set; }
+
+        public Pedra(int lado1, int lado2)
+        {
+            if (lado1 < 0 || lado1 > 6 || lado2 < 0 || lado2 > 6)
+                throw new ArgumentException("lado deve ser entre 0 e 6");
+
+            Lado1 = Math.Min(lado1, lado2);
+            Lado2 = Math.Max(lado1, lado2);
+        }
+
+        /// <summary>
+        /// Testa se a pedra pode encaixar com outra baseado nos pontos
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public bool PodeJogar(int p)
+        {
+            return Lado1 == p || Lado2 == p;
+        }
+
+        /// <summary>
+        /// Devolve qual é a ponta livre desta pedra, comparada à outra pedra
+        /// </summary>
+        /// <param name="pedra2"></param>
+        /// <returns></returns>
+        public int PontaLivre(Pedra pedra2)
+        {
+            if (Lado1 == pedra2.Lado1 || Lado1 == pedra2.Lado2) return Lado2;
+            if (Lado2 == pedra2.Lado1 || Lado2 == pedra2.Lado2) return Lado1;
+
+            // Pedras não tem lados iguais, devolve -1 como erro
+            return -1;
+        }
+
+        public static Pedra FromString(string pedra)
+        {
+            var lados = pedra.Split('x');
+            return new Pedra(Convert.ToInt32(lados[0]), Convert.ToInt32(lados[1]));
+        }
+
+        public override string ToString()
+        {
+            return $"{Lado1}x{Lado2}";
+        }
+    }
+}
